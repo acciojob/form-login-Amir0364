5322188dac835fdc92c6f00549def905510ae851
Mohd Amir <140543590+Amir0364@users.noreply.github.com>
2024-07-06 12:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	var firstName = document.getElementById('first').value;
+	var lastName = document.getElementById('last').value;
+	alert(`First Name: ${firstName}\nLast Name: ${lastName}`)
+	
 
 }
